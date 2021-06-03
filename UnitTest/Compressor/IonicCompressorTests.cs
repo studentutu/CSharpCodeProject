283a78f4dd3b736e599f20703100a4c27fa211cd
Mogoson <mogoson@outlook.com>
2021-06-04 02:52:01 +0800

    Add unit test for Compressors

---
+++ UnitTest/Compressor/IonicCompressorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MGS.Compress.Tests
+{
+    [TestClass()]
+    public class IonicCompressorTests
+    {
+        [TestMethod()]
+        public void CompressTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DecompressTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ UnitTest/Compressor/SharpCompressorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MGS.Compress.Tests
+{
+    [TestClass()]
+    public class SharpCompressorTests
+    {
+        [TestMethod()]
+        public void CompressTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DecompressTest()
+        {
+            Assert.Fail();
+        }
+    }
+}

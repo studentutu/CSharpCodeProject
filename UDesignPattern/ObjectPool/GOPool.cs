ccca5fa0e874198fcffe95916e352686bb88c697
Mogoson <mogoson@outlook.com>
2021-07-03 23:54:12 +0800

    Add UDesignPattern project

---
+++ UDesignPattern/ObjectPool/GOPool.cs
@@ -0,0 +1,121 @@
+﻿/*************************************************************************
+ *  Copyright © 2018-2019 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  GOPool.cs
+ *  Description  :  Define GameObjectPool.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  1.0
+ *  Date         :  2/9/2018
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using UnityEngine;
+
+namespace MGS.DesignPattern
+{
+    /// <summary>
+    /// Pool of gameobject.
+    /// </summary>
+    public class GOPool : ObjectPool<GameObject>
+    {
+        #region Field and Property
+        /// <summary>
+        /// Parent of gameobjects.
+        /// </summary>
+        public Transform Node { get; }
+
+        /// <summary>
+        /// Prefab to create clone.
+        /// </summary>
+        protected GameObject prefab;
+        #endregion
+
+        #region Public Method
+        /// <summary>
+        /// Create and initialize GameObjectPool.
+        /// </summary>
+        /// <param name="node">Parent of gameobjects.</param>
+        /// <param name="prefab">Prefab to create clone.</param>
+        /// <param name="maxCount">Max count limit of gameobjects in pool.</param>
+        public GOPool(Transform node, GameObject prefab, int maxCount = 100) : base(maxCount)
+        {
+            Node = node;
+            this.prefab = prefab;
+        }
+
+        /// <summary>
+        /// Take a gameobject from pool.
+        /// </summary>
+        /// <param name="position">Position of new gameobject.</param>
+        /// <param name="rotation">Rotation of new gameobject.</param>
+        /// <returns>A gameobject.</returns>
+        public virtual GameObject Take(Vector3 position, Quaternion rotation)
+        {
+            var obj = Take();
+            obj.transform.position = position;
+            obj.transform.rotation = rotation;
+            obj.SetActive(true);
+            return obj;
+        }
+
+        /// <summary>
+        /// Take a gameobject from pool.
+        /// </summary>
+        /// <param name="parent">Parent of new gameobject.</param>
+        /// <param name="localPosition">Local position of new gameobject.</param>
+        /// <param name="localRotation">Local rotation of new gameobject.</param>
+        /// <returns>A gameobject.</returns>
+        public virtual GameObject Take(Transform parent, Vector3 localPosition, Quaternion localRotation)
+        {
+            var obj = Take();
+            obj.transform.parent = parent;
+            obj.transform.localPosition = localPosition;
+            obj.transform.localRotation = localRotation;
+            obj.SetActive(true);
+            return obj;
+        }
+        #endregion
+
+        #region Protected Method
+        /// <summary>
+        /// Create new gameobject.
+        /// </summary>
+        /// <returns></returns>
+        protected override GameObject Create()
+        {
+            var clone = Object.Instantiate(prefab);
+            clone.transform.parent = Node;
+            return clone;
+        }
+
+        /// <summary>
+        /// Reset the gameobject.
+        /// </summary>
+        /// <param name="obj"></param>
+        protected override void Reset(GameObject obj)
+        {
+            obj.SetActive(false);
+
+            obj.tag = prefab.tag;
+            obj.layer = prefab.layer;
+
+            obj.transform.position = prefab.transform.position;
+            obj.transform.rotation = prefab.transform.rotation;
+
+            obj.transform.parent = null;
+            obj.transform.localScale = prefab.transform.localScale;
+            obj.transform.parent = Node;
+        }
+
+        /// <summary>
+        /// Dispose the gameobject.
+        /// </summary>
+        /// <param name="obj"></param>
+        protected override void Dispose(GameObject obj)
+        {
+            Object.Destroy(obj);
+        }
+        #endregion
+    }
+}
---
+++ UDesignPattern/ObjectPool/GOPoolManager.cs
@@ -0,0 +1,122 @@
+﻿/*************************************************************************
+ *  Copyright © 2018-2019 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  GOPoolManager.cs
+ *  Description  :  Manager of gameobject pool.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  1.0
+ *  Date         :  2/9/2018
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using MGS.Logger;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MGS.DesignPattern
+{
+    /// <summary>
+    /// Manager of gameobject pool.
+    /// </summary>
+    public sealed class GOPoolManager : Singleton<GOPoolManager>
+    {
+        #region Field and Property
+        /// <summary>
+        /// Dictionary store pools info(name and pool).
+        /// </summary>
+        private Dictionary<string, GOPool> poolsInfo = new Dictionary<string, GOPool>();
+
+        /// <summary>
+        /// Root transform for pools.
+        /// </summary>
+        private readonly Transform poolRoot;
+        #endregion
+
+        #region Private Method
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        private GOPoolManager()
+        {
+            //Create root for pools.
+            poolRoot = new GameObject("GameObjectPool").transform;
+            Object.DontDestroyOnLoad(poolRoot);
+        }
+        #endregion
+
+        #region Public Method
+        /// <summary>
+        /// Create a pool in this manager.
+        /// </summary>
+        /// <param name="name">Name of GameObjectPool.</param>
+        /// <param name="prefab">Prefab of GameObjectPool.</param>
+        /// <param name="maxCount">Max count limit of gameobjects in pool.</param>
+        /// <returns>Pool created base on parameters.</returns>
+        public GOPool CreatePool(string name, GameObject prefab, int maxCount = 100)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                LogUtility.LogError("Create pool error: The pool name can not be null or empty.");
+                return null;
+            }
+
+            if (poolsInfo.ContainsKey(name))
+            {
+                LogUtility.LogWarning("Create pool cancelled: The pool that name is \"{0}\" already exist in this manager.", name);
+                return poolsInfo[name];
+            }
+
+            if (prefab == null)
+            {
+                LogUtility.LogError("Create pool error: The prefab of pool can not be null.");
+                return null;
+            }
+
+            //Create new gameobject for pool.
+            var poolTrans = new GameObject(name).transform;
+            poolTrans.parent = poolRoot;
+
+            //Create new pool.
+            var newPool = new GOPool(poolTrans, prefab, maxCount);
+            poolsInfo.Add(name, newPool);
+            return newPool;
+        }
+
+        /// <summary>
+        /// Find GameObjectPool by name.
+        /// </summary>
+        /// <param name="name">Name of GameObjectPool.</param>
+        /// <returns>Name match GameObjectPool.</returns>
+        public GOPool FindPool(string name)
+        {
+            if (poolsInfo.ContainsKey(name))
+            {
+                return poolsInfo[name];
+            }
+            else
+            {
+                LogUtility.LogWarning("Find pool error: The pool that name is \"{0}\" does not exist in this manager.", name);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Delete GameObjectPool by name.
+        /// </summary>
+        /// <param name="name">Name of GameObjectPool.</param>
+        public void DeletePool(string name)
+        {
+            if (poolsInfo.ContainsKey(name))
+            {
+                Object.Destroy(poolsInfo[name].Node.gameObject);
+                poolsInfo.Remove(name);
+            }
+            else
+            {
+                LogUtility.LogWarning("Delete pool error: The pool that name is \"{0}\" does not exist in this manager.", name);
+            }
+        }
+        #endregion
+    }
+}

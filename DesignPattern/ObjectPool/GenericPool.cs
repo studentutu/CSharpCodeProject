61859327c04ed7d6e213d19af2e2f3c20a632e66
Mogoson <mogoson@outlook.com>
2021-07-04 15:49:03 +0800

    Add GenericPool

---
+++ DesignPattern/ObjectPool/GenericPool.cs
@@ -0,0 +1,70 @@
+﻿/*************************************************************************
+ *  Copyright © 2021 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  GenericPool.cs
+ *  Description  :  Generic object pool.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  1.0
+ *  Date         :  7/4/2021
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using System;
+
+namespace MGS.DesignPattern
+{
+    /// <summary>
+    /// Interface for resettable object.
+    /// </summary>
+    public interface IResettable : IDisposable
+    {
+        /// <summary>
+        /// Reset object.
+        /// </summary>
+        void Reset();
+    }
+
+    /// <summary>
+    /// Generic object pool.
+    /// </summary>
+    public class GenericPool<T> : ObjectPool<T> where T : IResettable, new()
+    {
+        #region Public Method
+        /// <summary>
+        /// Constructor of GenericPool.
+        /// </summary>
+        /// <param name="maxCount">Max count limit of objects in pool.</param>
+        public GenericPool(int maxCount = 100) : base(maxCount) { }
+        #endregion
+
+        #region Protected Method
+        /// <summary>
+        /// Create a new object.
+        /// </summary>
+        /// <returns></returns>
+        protected override T Create()
+        {
+            return new T();
+        }
+
+        /// <summary>
+        /// Reset this object.
+        /// </summary>
+        /// <param name="obj"></param>
+        protected override void Reset(T obj)
+        {
+            obj.Reset();
+        }
+
+        /// <summary>
+        /// Dispose this object.
+        /// </summary>
+        /// <param name="obj"></param>
+        protected override void Dispose(T obj)
+        {
+            obj.Dispose();
+        }
+        #endregion
+    }
+}

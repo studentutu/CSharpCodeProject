9fc1dcd62618598b18bc38764bd5b82bb99203a5
Mogoson <mogoson@outlook.com>
2021-07-05 23:14:55 +0800

    Update head notes

--- Media/Subtitle/SRTSubtitle.cs
+++ Media/Subtitle/SRTSubtitle.cs
@@ -1,5 +1,5 @@
-/*************************************************************************
- *  Copyright ? 2019 Mogoson. All rights reserved.
+﻿/*************************************************************************
+ *  Copyright © 2019 Mogoson. All rights reserved.
  *------------------------------------------------------------------------
  *  File         :  SRTSubtitle.cs
  *  Description  :  SRT subtitle of video.

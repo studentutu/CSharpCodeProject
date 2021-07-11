b7bbdcf93ea7a3d47ec039890e61d1f32239e5cd
Mogoson <mogoson@outlook.com>
2021-07-11 23:13:37 +0800

    Add UDesignPattern csproj

---
+++ UDesignPattern/Singleton/SingleBehaviour.cs
@@ -0,0 +1,68 @@
+﻿/*************************************************************************
+ *  Copyright © 2021 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  SingleBehaviour.cs
+ *  Description  :  Generic single behaviour.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  1.0
+ *  Date         :  7/11/2021
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using System;
+
+namespace MGS.DesignPattern
+{
+    /// <summary>
+    /// Generic single behaviour.
+    /// Auto create the generic instance, do not add this component to any GameObject by yourself.
+    /// </summary>
+    public sealed class SingleBehaviour : SingleComponent<SingleBehaviour>
+    {
+        #region
+        /// <summary>
+        /// Event on application focus.
+        /// </summary>
+        public event Action<bool> OnApplicationFocusEvent;
+
+        /// <summary>
+        /// Event on application pause.
+        /// </summary>
+        public event Action<bool> OnApplicationPauseEvent;
+
+        /// <summary>
+        /// Event on application quit.
+        /// </summary>
+        public event Action OnApplicationQuitEvent;
+        #endregion
+
+        #region
+        /// <summary>
+        /// On application focus.
+        /// </summary>
+        /// <param name="hasFocus"></param>
+        void OnApplicationFocus(bool hasFocus)
+        {
+            OnApplicationFocusEvent?.Invoke(hasFocus);
+        }
+
+        /// <summary>
+        /// On application pause.
+        /// </summary>
+        /// <param name="pauseStatus"></param>
+        void OnApplicationPause(bool pauseStatus)
+        {
+            OnApplicationPauseEvent?.Invoke(pauseStatus);
+        }
+
+        /// <summary>
+        /// On application quit.
+        /// </summary>
+        void OnApplicationQuit()
+        {
+            OnApplicationQuitEvent?.Invoke();
+        }
+        #endregion
+    }
+}
---
+++ UDesignPattern/Singleton/SingleComponent.cs
@@ -0,0 +1,53 @@
+﻿/*************************************************************************
+ *  Copyright © 2021 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  SingleComponent.cs
+ *  Description  :  Generic base class for single Component.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  1.0
+ *  Date         :  7/11/2021
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using UnityEngine;
+
+namespace MGS.DesignPattern
+{
+    /// <summary>
+    /// Generic base class for single Component.
+    /// </summary>
+    [DisallowMultipleComponent]
+    public abstract class SingleComponent<T> : MonoBehaviour where T : Component
+    {
+        /// <summary>
+        /// Single instance of the specified Component T.
+        /// </summary>
+        public static T Instance { get { return Agent.instance; } }
+
+        #region
+        /// <summary>
+        /// Agent provide the single instance.
+        /// </summary>
+        private class Agent
+        {
+            #region Property
+            /// <summary>
+            /// Single instance of the specified Component T.
+            /// </summary>
+            internal static readonly T instance = new GameObject(typeof(T).Name).AddComponent<T>();
+            #endregion
+
+            #region Static Method
+            /// <summary>
+            /// Explicit static constructor to tell C# compiler not to mark type as beforefieldinit.
+            /// </summary>
+            static Agent()
+            {
+                DontDestroyOnLoad(instance);
+            }
+            #endregion
+        }
+        #endregion
+    }
+}

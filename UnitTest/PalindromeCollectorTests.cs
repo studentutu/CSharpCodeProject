95f02f1e0ec0ad4f4ec17d0d1d1dbbc043f6f2b3
Mogoson <mogoson@outlook.com>
2021-08-16 23:16:15 +0800

    Add test method

---
+++ UnitTest/PalindromeCollectorTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MGS.Algorithm.Tests
+{
+    [TestClass()]
+    public class PalindromeCollectorTests
+    {
+        [TestMethod()]
+        public void GetLongestPalindromeTest()
+        {
+            var str = "12321";
+            var length = PalindromeCollector.GetLongestPalindrome(str);
+
+            Assert.IsTrue(length == 5);
+            Console.WriteLine("Max palindrome length of {0} is {1}", str, length);
+        }
+
+        [TestMethod()]
+        public void GetLongestPalindrome0Test()
+        {
+            var str = "12321";
+            var length = PalindromeCollector.GetLongestPalindrome0(str);
+
+            Assert.IsTrue(length == 5);
+            Console.WriteLine("Max palindrome length of {0} is {1}", str, length);
+        }
+
+        [TestMethod()]
+        public void GetTheLongestPalindromeTest()
+        {
+            var str = "asd12321gfs7887";
+            var pldm = PalindromeCollector.GetTheLongestPalindrome(str);
+
+            Assert.IsTrue(pldm == "12321");
+            Console.WriteLine("The longest palindrome of {0} is {1}", str, pldm);
+        }
+
+        [TestMethod()]
+        public void CollectPalindromesTest()
+        {
+            var str = "asd12321gfs7887";
+            var pldms = PalindromeCollector.CollectPalindromes(str);
+            Assert.IsNotNull(pldms);
+
+            foreach (var pldm in pldms)
+            {
+                Console.WriteLine("pldm {0}", pldm);
+            }
+        }
+    }
+}
---
+++ UnitTest/WaterCollectorTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MGS.Algorithm.Tests
+{
+    [TestClass()]
+    public class WaterCollectorTests
+    {
+        [TestMethod()]
+        public void CollectWaterTest()
+        {
+            var ps = new int[] { 1, 2, 3, 1, 1, 3, 2, 1 };
+            var w = WaterCollector.CollectWater(ps);
+            Assert.IsTrue(w == 4);
+            Console.WriteLine("water {0}", w);
+        }
+
+        [TestMethod()]
+        public void CollectWatersTest()
+        {
+            var ps = new int[] { 1, 2, 3, 1, 1, 3, 2, 1 };
+            var ws = WaterCollector.CollectWaters(ps);
+            Assert.IsNotNull(ws);
+            Assert.IsTrue(ws.Count == 2);
+
+            foreach (var w in ws)
+            {
+                Console.WriteLine("index {0} water {1}", w.Key, w.Value);
+            }
+        }
+    }
+}

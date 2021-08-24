8484d1a4a47e6781719070ae35593819c36f2d38
Mogoson <mogoson@outlook.com>
2021-08-25 00:51:05 +0800

    Implement UIPositioner

--- UGUI/Handler/UIPositioner.cs
+++ UGUI/Handler/UIPositioner.cs
@@ -10,6 +10,9 @@
  *  Description  :  Initial development version.
  *************************************************************************/
 
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace MGS.UGUI
 {
     /// <summary>
@@ -17,6 +20,41 @@ namespace MGS.UGUI
     /// </summary>
     public class UIPositioner : UIHandler
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="worldPos"></param>
+        /// <param name="offset"></param>
+        /// <param name="alignment"></param>
+        public void SetPosition(Vector3 worldPos, Vector2 offset, TextAnchor alignment = TextAnchor.MiddleCenter)
+        {
+            //Set pivot.
+            var originPivot = RectTrans.pivot;
+            var tempPivot = Text.GetTextAnchorPivot(alignment);
+            RectTrans.pivot = tempPivot;
 
+            //Set position and offset.
+            transform.position = worldPos;
+            var anchorPos = RectTrans.anchoredPosition;
+            anchorPos += offset;
+            RectTrans.anchoredPosition = anchorPos;
+
+            //Revert pivot.
+            RectTrans.pivot = originPivot;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="camera"></param>
+        /// <param name="screenPos"></param>
+        /// <param name="offset"></param>
+        /// <param name="alignment"></param>
+        public void SetPosition(Camera camera, Vector3 screenPos, Vector2 offset, TextAnchor alignment = TextAnchor.MiddleCenter)
+        {
+            //Convert to world position.
+            var worldPos = camera.ScreenToWorldPoint(screenPos);
+            SetPosition(worldPos, offset, alignment);
+        }
     }
 }

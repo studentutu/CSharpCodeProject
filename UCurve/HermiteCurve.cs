e92e70c337f6ff7f6ee269fbae4edd4c4af1ea54
Mogoson <mogoson@outlook.com>
2021-09-01 02:43:35 +0800

    Implement HermiteCurve

--- UCurve/HermiteCurve.cs
+++ UCurve/HermiteCurve.cs
@@ -53,7 +53,7 @@ namespace MGS.UCurve
         /// Constructor.
         /// </summary>
         /// <param name="frames">Key frames of curve.</param>
-        public HermiteCurve(KeyFrame[] frames)
+        public HermiteCurve(IEnumerable<KeyFrame> frames)
         {
             this.frames.AddRange(frames);
         }
@@ -72,7 +72,7 @@ namespace MGS.UCurve
         /// </summary>
         /// <param name="time">Key of frame.</param>
         /// <param name="value">Value of frame.</param>
-        public void AddFrame(double time, Vector3 value)
+        public void AddFrame(float time, Vector3 value)
         {
             frames.Add(new KeyFrame(time, value));
         }
@@ -96,13 +96,91 @@ namespace MGS.UCurve
         }
 
         /// <summary>
+        /// Clear frames.
+        /// </summary>
+        public void ClearFrames()
+        {
+            frames.Clear();
+        }
+
+        /// <summary>
+        /// Smooth in and out tangents.
+        /// </summary>
+        /// <param name="weight"></param>
+        public void SmoothTangents(float weight)
+        {
+            for (int i = 0; i < frames.Count; i++)
+            {
+                SmoothTangents(i, weight);
+            }
+        }
+
+        /// <summary>
+        /// Smooth in and out tangents.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="weight"></param>
+        public void SmoothTangents(int index, float weight)
+        {
+            //Designed By Mogoson.
+            var pointers = new List<int>();
+            for (int i = -1; i < 2; i++)
+            {
+                var pointer = index + i;
+                if (pointer < 0)
+                {
+                    continue;
+                }
+                else if (pointer >= frames.Count)
+                {
+                    break;
+                }
+                pointers.Add(pointer);
+            }
+
+            if (pointers.Count < 2)
+            {
+                return;
+            }
+
+            if (pointers.Count == 2)
+            {
+                int p0 = pointers[0];
+                var p1 = pointers[1];
+                var k0 = frames[p0];
+                var k1 = frames[p1];
+
+                var t = (k1.value - k0.value) / (k1.time - k0.time);
+                var k = (p0 == index ? k0 : k1);
+                k.inTangent = k.outTangent = t;
+                frames[index] = k;
+            }
+            else
+            {
+                var p0 = pointers[0];
+                var p1 = pointers[1];
+                var p2 = pointers[2];
+                var k0 = frames[p0];
+                var k1 = frames[p1];
+                var k2 = frames[p2];
+
+                var weight01 = (1 + weight) / 2;
+                var weight12 = (1 - weight) / 2;
+                var t01 = (k1.value - k0.value) / (k1.time - k0.time);
+                var t12 = (k2.value - k1.value) / (k2.time - k1.time);
+                k1.inTangent = k1.outTangent = t01 * weight01 + t12 * weight12;
+                frames[index] = k1;
+            }
+        }
+
+        /// <summary>
         /// Evaluate the curve at t.
         /// </summary>
         /// <param name="t"></param>
         /// <returns></returns>
         public Vector3 Evaluate(float t)
         {
-            return Vector3.zero;
+            return Evaluate(frames.ToArray(), t);
         }
         #endregion
 
@@ -113,47 +191,76 @@ namespace MGS.UCurve
         /// <param name="frames">Key frames of hermite curve.</param>
         /// <param name="t"></param>
         /// <returns>The value of hermite curve at time.</returns>
-        public static Vector3 Evaluate(KeyFrame[] frames, double t)
+        public static Vector3 Evaluate(KeyFrame[] frames, float t)
         {
             if (frames == null || frames.Length == 0)
             {
                 return Vector3.zero;
             }
 
-            var value = Vector3.zero;
             if (frames.Length == 1)
             {
-                value = frames[0].value;
+                return frames[0].value;
             }
-            else
+
+            if (t <= frames[0].time)
             {
-                if (t <= frames[0].time)
+                return frames[0].value;
+            }
+
+            if (t >= frames[frames.Length - 1].time)
+            {
+                return frames[frames.Length - 1].value;
+            }
+
+            var fr = 0;
+            var to = frames.Length;
+            while (fr < to)
+            {
+                var hf = fr + (to - fr) / 2;
+                if (frames[hf].time == t)
                 {
-                    value = frames[0].value;
+                    return frames[hf].value;
                 }
-                else if (t >= frames[frames.Length - 1].time)
+
+                if (frames[hf].time < t)
                 {
-                    value = frames[frames.Length - 1].value;
+                    if (frames[hf + 1].time == t)
+                    {
+                        return frames[hf + 1].value;
+                    }
+
+                    if (frames[hf + 1].time > t)
+                    {
+                        return Evaluate(frames[hf], frames[hf + 1], t);
+                    }
+
+                    if (fr == hf)
+                    {
+                        return Vector3.zero;
+                    }
+                    fr = hf;
                 }
                 else
                 {
-                    for (int i = 0; i < frames.Length; i++)
+                    if (frames[hf - 1].time == t)
                     {
-                        if (i == frames[i].time)
-                        {
-                            value = frames[i].value;
-                            break;
-                        }
-
-                        if (t > frames[i].time && t < frames[i + 1].time)
-                        {
-                            value = Evaluate(frames[i], frames[i + 1], t);
-                            break;
-                        }
+                        return frames[hf - 1].value;
                     }
+
+                    if (frames[hf - 1].time < t)
+                    {
+                        return Evaluate(frames[hf - 1], frames[hf], t);
+                    }
+
+                    if (to == hf)
+                    {
+                        return Vector3.zero;
+                    }
+                    to = hf;
                 }
             }
-            return value;
+            return Vector3.zero;
         }
 
         /// <summary>
@@ -163,9 +270,68 @@ namespace MGS.UCurve
         /// <param name="end">End Key frame of hermite curve.</param>
         /// <param name="t">Time of curve to evaluate value.</param>
         /// <returns>The value of hermite curve at time on the range from start key frame to end key frame.</returns>
-        public static Vector3 Evaluate(KeyFrame start, KeyFrame end, double t)
+        public static Vector3 Evaluate(KeyFrame start, KeyFrame end, float t)
         {
-            return Vector3.zero;
+            return Evaluate(start.time, start.value, start.outTangent, end.time, end.value, end.inTangent, t);
+        }
+
+        /// <summary>
+        /// Hermite interpolate between t0 and t1 by t.
+        /// </summary>
+        /// <param name="t0">Time of t0.</param>
+        /// <param name="v0">Value of t0.</param>
+        /// <param name="m0">Micro quotient value of t0.</param>
+        /// <param name="t1">Time of t1.</param>
+        /// <param name="v1">Value of t1.</param>
+        /// <param name="m1">Micro quotient value of t1.</param>
+        /// <param name="t">t is between t0 and t1.</param>
+        /// <returns></returns>
+        public static Vector3 Evaluate(float t0, Vector3 v0, Vector3 m0, float t1, Vector3 v1, Vector3 m1, float t)
+        {
+            /*  Designed By Mogoson.
+             *  Hermite Polynomial Structure
+             *  Base: H(t) = v0a0(t) + v1a1(t) + m0b0(t) + m1b1(t)
+             * 
+             *                     t-t0    t-t1  2
+             *        a0(t) = (1+2------)(------)
+             *                     t1-t0   t0-t1
+             *                    
+             *                     t-t1    t-t0  2
+             *        a1(t) = (1+2------)(------)
+             *                     t0-t1   t1-t0
+             * 
+             *                        t-t1  2
+             *        b0(t) = (t-t0)(------)
+             *                        t0-t1
+             * 
+             *                        t-t0  2
+             *        b1(t) = (t-t1)(------)
+             *                        t1-t0
+             * 
+             *  Let:  d0 = t-t0, d1 = t-t1, d = t0-t1
+             * 
+             *              d0          d1
+             *        q0 = ---- , q1 = ----
+             *              d           d
+             * 
+             *               t-t1  2     d1  2     2          t-t0  2     d0  2     2
+             *        p0 = (------)  = (----)  = q1  , p1 = (------)  = (----)  = q0
+             *               t0-t1       d                    t1-t0       -d
+             * 
+             *  Get:  H(t) = (1-2q0)v0p0 + (1+2q1)v1p1 + mod0p0 + m1d1p1
+             */
+
+            var d0 = t - t0;
+            var d1 = t - t1;
+            var d = t0 - t1;
+
+            var q0 = d0 / d;
+            var q1 = d1 / d;
+
+            var p0 = q1 * q1;
+            var p1 = q0 * q0;
+
+            return (1 - 2 * q0) * v0 * p0 + (1 + 2 * q1) * v1 * p1 + m0 * d0 * p0 + m1 * d1 * p1;
         }
         #endregion
     }
@@ -180,7 +346,7 @@ namespace MGS.UCurve
         /// <summary>
         /// Time of frame.
         /// </summary>
-        public double time;
+        public float time;
 
         /// <summary>
         /// Value of frame.
@@ -204,7 +370,7 @@ namespace MGS.UCurve
         /// </summary>
         /// <param name="time">Time of frame.</param>
         /// <param name="value">Value of frame.</param>
-        public KeyFrame(double time, Vector3 value)
+        public KeyFrame(float time, Vector3 value)
         {
             this.time = time;
             this.value = value;
@@ -219,7 +385,7 @@ namespace MGS.UCurve
         /// <param name="value">Value of frame.</param>
         /// <param name="inTangent">In tangent of frame.</param>
         /// <param name="outTangent">Out tangent of frame.</param>
-        public KeyFrame(double time, Vector3 value, Vector3 inTangent, Vector3 outTangent)
+        public KeyFrame(float time, Vector3 value, Vector3 inTangent, Vector3 outTangent)
         {
             this.time = time;
             this.value = value;
